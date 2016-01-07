578b5779e53d0adebcdcd4832f8d37cd3e3d1086
glorioust <tolyaniy@gmail.com>
2016-01-07 21:32:00 +0100

    adding rebase script

---
+++ rebase-no-conflicts.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+function rebase_with_temporary_commit {
+	echo "Changes detected, commiting to temporary commit"
+	git commit -a -m "temporary commit"
+	rebase_on_latest_master
+	git reset --soft HEAD~1
+}
+
+function rebase_on_latest_master {
+	echo "No changes detected, rebasing"
+	git checkout master
+	git -c core.quotepath=false fetch origin --progress --prune
+	git checkout -
+	git rebase master
+}
+
+PROJECT_PATH=
+set -o history
+# set -o verbose
+cd $PROJECT_PATH
+
+output="$(git status -uno)"
+echo $output
+if [[ $output != *"nothing to commit"* ]]; then
+	rebase_with_temporary_commit
+else
+	rebase_on_latest_master
+fi
+echo "THE END"
